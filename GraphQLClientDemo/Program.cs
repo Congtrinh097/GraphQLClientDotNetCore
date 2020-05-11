fcca66fc4f6e30bbcb14f06a0a21cb259e12eb6b
Trinh Tran Cong <trinh.tran@orientsoftware.com>
2020-05-11 11:23:36 +0700

    Add project files.

---
+++ GraphQLClientDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using GraphQL;
+using System;
+
+namespace GraphQLClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var graphQLClient = new GraphQLRequest
+            {
+                Query = @"
+                    query PersonAndFilms($id: ID) {
+                        person(id: $id) {
+                            name
+                            filmConnection {
+                                films {
+                                    title
+                                }
+                            }
+                        }
+                    }",
+                OperationName = "PersonAndFilms",
+                Variables = new
+                {
+                    id = "cGVvcGxlOjE="
+                }
+            };
+
+            var graphQLResponse = await graphQLClient.SendQueryAsync<ResponseType>(personAndFilmsRequest);
+        }
+    }
+}
